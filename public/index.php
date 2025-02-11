39772290a889f9d3306c0a6d7a17b8854dad02fa
crlstve <ecarles10@gmail.com>
2025-02-11 09:45:56 +0100

    Tailwindcss4 init

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../src/output.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <main class="flex flex-col items-center justify-center">
+    <p class="text-2xl md:text-3xl xl:text-4xl font-bold text-red-500">Test</p>
+  </main>
+</body>
+</html>
