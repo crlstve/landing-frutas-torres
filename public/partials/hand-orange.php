06558202092b85f6e427457c2953306b4a37fff2
crlstve <ecarles10@gmail.com>
2025-02-12 12:47:28 +0100

    orange

--- public/partials/hand-orange.php
+++ public/partials/hand-orange.php
@@ -1,7 +1,11 @@
-<section class="w-full flex flex-col bg-beige -mt-36 rounded-t-[8rem] pt-56">
-    <div class="w-full container">
-        <p class="color-green text-lg font-semibold">
+<section class="w-full flex flex-col bg-beige -mt-36 rounded-t-[6rem] lg:rounded-t-[8rem] py-32 relative">
+
+    <div id="hand" class="w-full container mx-auto py-24 flex flex-row justify-center">
+
+        <p class="color-green text-lg lg:text-xl font-semibold w-full md:w-6/12 lg:ml-32">
             El sabor de siempre está en peligro de extinción, porque que la fruta no sepa a fruta se ha vuelto algo habitual para las nuevas generaciones. Pero… ¿Cómo explicarles a nuestros peques cómo eran aquellos tiempos de cuando las naranjas sabían a naranjas? 
         </p>
+
     </div>
+
 </section>
--- public/partials/hero.php
+++ public/partials/hero.php
@@ -1,14 +1,14 @@
 <div  id="hero" class="w-full justify-center" style="background: linear-gradient(180deg, rgba(2, 0, 36, 0) 50%, rgba(15, 92, 64, 1) 100%),url(./assets/img/bg-hero.jpg);background-size: cover;background-position: top center;">
 
-    <header class="w-full rounded-t-0 rounded-b-full bg-green z-50">
+    <header class="w-full rounded-t-0 rounded-b-[6rem] lg:rounded-b-full bg-green z-50">
         <div class="w-full py-8 container flex flex-row items-center justify-center mx-auto">
-            <img src="./assets/img/torres-logo.png" alt="logo" class="w-full md:w-1/12">
+            <img src="./assets/img/torres-logo.png" alt="logo" class="w-32 lg:w-1/12">
         </div>
     </header>
 
-    <section class="w-full py-4 container flex flex-row items-end justify-between background-image mx-auto mt-24 mb-44 " >
+    <section class="w-full py-4 container flex flex-col lg:flex-row items-end justify-between background-image mx-auto mt-24 mb-44 " >
                     
-            <div class="w-full md:w-3/12 flex flex-col z-50">
+            <div class="w-full lg:w-3/12 flex flex-col z-50">
                 <p class="text-center text-lg text-white">
                     <?= mb_strtoupper("Una canción para difundir el sabor que tiene que ser."); ?>
                 </p>
@@ -17,39 +17,37 @@
                 </p>
             </div>
 
-            <div class="bg-orange rounded-3xl text-center w-full md:w-fit md:max-w-4/12 px-8 pt-8 pb-2 -mb-32 z-50">
+            <div class="bg-orange rounded-3xl text-center w-full lg:w-fit lg:max-w-4/12 px-8 pt-8 pb-2 -mb-32 z-50">
 
-                <h1 class="text-white text-3xl md:text-8xl leading-none text-balance ">PRESUME DE FRUTA</h1>
+                <h1 class="text-white text-3xl lg:text-8xl leading-none text-balance ">PRESUME DE FRUTA</h1>
 
             </div>
 
+            <div class="w-full lg:w-[28%] flex flex-col bg-white px-10 py-8 rounded-3xl gap-6 z-50">
+                <p class="text-center color-green px-7 text-lg">
+                    Inscribe a tu colegio rellenando el formulario. En breve, contactaremos contigo.
+                </p>
+                <a href="" target="_blank" class="text-center color-green underline px-7 text-lg">
+                    Descarga aquí la letra e instrumental de la canción
+                </a>
 
-
-        <div class="w-full md:w-[28%] flex flex-col bg-white px-10 py-8 rounded-3xl gap-6 z-50">
-            <p class="text-center color-green px-7 text-lg">
-                Inscribe a tu colegio rellenando el formulario. En breve, contactaremos contigo.
-            </p>
-            <a href="" target="_blank" class="text-center color-green underline px-7 text-lg">
-                Descarga aquí la letra e instrumental de la canción
-            </a>
-
-            <form ie="form" action="submit.php" method="post" class="flex flex-col gap-4 ">
-                <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
-                <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
-                <input type="phone" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
-                <input type="email" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
-                <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
-                <button class="bg-green text-white px-4 py-2 rounded-sm font-bold">Mi cole quiere participar</button>
-                <div>
-                    <input id="bases" type="checkbox" class="rounded-sm border border-gray-200 py-2 px-4 text-slate-200" palceholder="Centro educativo">
-                    <label for="bases" class="text-slate-400 text-sm">He leído y acepto las bases del concurso.</label>
-                </div>
-                <div>
-                    <input id="bases" type="checkbox" class="rounded-sm border border-gray-200 py-2 px-4 text-slate-200" palceholder="Centro educativo">
-                    <label for="bases">He leído y acepto las políticas de privacidad.</label>
-                </div>
-            </form>
-        </div>
+                <form ie="form" action="submit.php" method="post" class="flex flex-col gap-4 ">
+                    <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
+                    <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
+                    <input type="phone" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
+                    <input type="email" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
+                    <input type="text" class="rounded-sm border border-gray-200 py-4 px-6 text-slate-200 w-full" palceholder="Centro educativo">
+                    <button class="bg-green text-white px-4 py-2 rounded-sm font-bold">Mi cole quiere participar</button>
+                    <div>
+                        <input id="bases" type="checkbox" class="rounded-sm border border-gray-200 py-2 px-4 text-slate-200" palceholder="Centro educativo">
+                        <label for="bases" class="text-slate-400 text-sm">He leído y acepto las bases del concurso.</label>
+                    </div>
+                    <div>
+                        <input id="bases" type="checkbox" class="rounded-sm border border-gray-200 py-2 px-4 text-slate-200" palceholder="Centro educativo">
+                        <label for="bases">He leído y acepto las políticas de privacidad.</label>
+                    </div>
+                </form>
+            </div>
 
     </section>
 
