713285c5139ac546fb5848d0ff0f989c4164e3ec
crlstve <ecarles10@gmail.com>
2025-02-14 11:33:47 +0100

    favicon bien

--- public/partials/head.php
+++ public/partials/head.php
@@ -6,12 +6,11 @@
             <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="author" content="c.esteve">
-            <meta name="favicon" content="https://frutastorres.com/wp-content/uploads/2024/01/cropped-cropped-favicon-192x192-1-32x32.png">
             <meta name="keyword" content="frutas, torres">
             <meta name="description" content="Frutas Torres presenta presume de frutas con Torres, un concurso para tu centro escolar.">
             <title>Presume de frutas con Torres</title>
             <link rel="canonical" href="https://presumedefrutascontorres.com" />
-            <link rel="icon" type="image/x-icon" href="">
+            <link rel="icon" type="image/x-icon" href="https://frutastorres.com/wp-content/uploads/2024/01/cropped-cropped-favicon-192x192-1-32x32.png">
             <meta property="og:locale" content="es_ES" />
             <meta property="og:type" content="website" />
             <meta property="og:title" content="Presume de frutas con Torres" />
